cc96ee6d53c1d8ea1077fa640ec05d3cdce406ad
TANVI SHETTY <tanvishetty0610@gmail.com>
2026-05-16 14:17:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,29 @@ while True:
     print("\nTransaction History:")
     for iytem in expenses:
       print(item)
-  elif choice==3:
-    credit=0
-    debit=0
+  elif choice == "3":
+        credit = 0
+        debit = 0
+        category_totals = {}
     for item in expenses:
-      if item["type"].lower()=="credit":
-        credit+=item["amount"]
+      if item["type"].lower() == "credit":
+        credit += item["amount"]
       else:
-        debit+=item["amount"]
-    balance=credit-debit
-    print("Total Credit:Rs",credit)
-    print("Total Debit:Rs",debit)
-    print("Remaining Balance:Rs",balance)
+        debit += item["amount"]
+        category = item["category"]
+        if category in category_totals:
+          category_totals[category] += item["amount"]
+        else:
+          category_totals[category] = item["amount"]
+    balance = credit - debit
+    print(f"\nTotal Credit: Rs.{credit}")
+    print(f"Total Debit: Rs.{debit}")
+    print(f"Remaining Balance: Rs.{balance}")
+    if category_totals:
+      highest = max(category_totals, key=category_totals.get)
+      lowest = min(category_totals, key=category_totals.get)
+      print(f"Highest Spending Category: {highest}")
+      print(f"Lowest Spending Category: {lowest}")
   elif choice==4:
     print("Thank you!")
     break
