4eeda676a762e3042339d9ba4998e470c748d2d6
TANVI SHETTY <tanvishetty0610@gmail.com>
2026-05-17 13:21:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
-expenses=[]
-while True:
-  print("\nSMART EXPENSE TRACKER")
-  print("\n1.Add Transactions")
-  print("\n2.View Transactions")
-  print("\n3.Show Balance")
-  print("\n4.Exit")
-  choice=int(input("Enter your choice:"))
-  if choice==1:
-    amount=float(input("Enter amount:"))
-    category=input("Enter category:")
-    transaction_type=input("Credit or Debit:")
-    date=input("Enter date (DD/MM/YYYY):")
-    transaction={"amount":amount,"category":category,"type":transaction_type,"date":date}
-    expensesappend(transaction)
-    print("Transaction added successfully!")
-  elif choice==2:
-    print("\nTransaction History:")
-    for iytem in expenses:
-      print(item)
-  elif choice == "3":
-        credit = 0
-        debit = 0
-        category_totals = {}
-    for item in expenses:
-      if item["type"].lower() == "credit":
-        credit += item["amount"]
-      else:
-        debit += item["amount"]
-        category = item["category"]
-        if category in category_totals:
-          category_totals[category] += item["amount"]
-        else:
-          category_totals[category] = item["amount"]
-    balance = credit - debit
-    print(f"\nTotal Credit: Rs.{credit}")
-    print(f"Total Debit: Rs.{debit}")
-    print(f"Remaining Balance: Rs.{balance}")
-    if category_totals:
-      highest = max(category_totals, key=category_totals.get)
-      lowest = min(category_totals, key=category_totals.get)
-      print(f"Highest Spending Category: {highest}")
-      print(f"Lowest Spending Category: {lowest}")
-  elif choice==4:
-    print("Thank you!")
-    break
-  else:
-    print("Invalid choice!")
+import streamlit as st
+import pandas as pd
+import os
+st.title("Smart Expense Tracker")
+file = "expenses.csv"
+if os.path.exists(file):
+  df = pd.read_csv(file)
+else:
+  df = pd.DataFrame(columns=["Type", "Category", "Amount"]
+                      
+expense_type = st.selectbox("Type", ["Income", "Expense"])
+
+category = st.text_input("Category")
+
+amount = st.number_input("Amount", min_value=0.0)
+
+if st.button("Add Transaction"):
+  new_data = {"Type": expense_type,"Category": category,"Amount": amount}
+  df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+  df.to_csv(file, index=False)
+  st.success("Transaction Added Successfully!")
+
+st.subheader("Transactions")
+
+st.dataframe(df)
+
+income = df[df["Type"] == "Income"]["Amount"].sum()
+
+expense = df[df["Type"] == "Expense"]["Amount"].sum()
+
+balance = income - expense
+
+st.subheader("Summary")
+
+st.write("Total Income:", income)
+
+st.write("Total Expense:", expense)
+
+st.write("Balance:", balance)
